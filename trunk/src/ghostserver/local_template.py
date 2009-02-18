921f92d3cda4e6efe230aafb6f60918c428e6bd6
Gustavo Andrés Angulo <woakas@pehik.co>
2009-02-18 01:09:27 +0000

    Modificacíon del local_template y el manejo del Leame.lst
    git-svn-id: https://svn.ghost.webhop.org:8443@17 7a9112b4-9480-4b6a-83bd-0f5d1dd4322e

--- trunk/src/ghostserver/local_template.py
+++ trunk/src/ghostserver/local_template.py
@@ -1,8 +1,8 @@
 
 import os
 DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'ghost'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'garbage'             # Not used with sqlite3.
+DATABASE_NAME = 'ghostBD'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'ghost'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'pepito'         # Not used with sqlite3.
 DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
@@ -26,6 +26,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.databrowse',
+    'ghostserver.ghost_lbs'
 #    'django.contrib.contenttypes',
 
 
